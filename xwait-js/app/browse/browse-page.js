b6954f2a1533994dc4883ae8ea38a9cabe5baa96
MonacoJoseph <jcm132@pitt.edu>
2019-10-12 16:28:02 -0400

    Adding files lol

--- xwait-js/app/browse/browse-page.js
+++ xwait-js/app/browse/browse-page.js
@@ -1,8 +1,26 @@
 const BrowseViewModel = require("./browse-view-model");
+const http = require('tns-core-modules/http');
 
 function onNavigatingTo(args) {
-    const component = args.object;
-    component.bindingContext = new BrowseViewModel();
+    //const component = args.object;
+    //component.bindingContext;
 }
 
+function closeModal(args) {
+    const view = args.object;
+    console.dir(args);
+
+    http.request({
+        url: "http://35.231.209.3/checkOut",
+        method: "GET"
+    }).then((res) => {
+        console.log(res['content']);
+        resp = res['content'];
+    }, (e) => {
+        console.log(e);
+    });
+    view.closeModal();
+}
+
+exports.closeModal = closeModal;
 exports.onNavigatingTo = onNavigatingTo;
--- xwait-js/app/home/home-items-page.js
+++ xwait-js/app/home/home-items-page.js
@@ -1,26 +1,66 @@
 const HomeItemsViewModel = require("./home-items-view-model");
+const http = require('tns-core-modules/http');
+//import {EventData,Observable} from "data/observable";
 
+let resp = 0;
 function onNavigatingTo(args) {
     const component = args.object;
     component.bindingContext = new HomeItemsViewModel();
 }
 
-function onItemTap(args) {
-    const view = args.view;
-    const page = view.page;
-    const tappedItem = view.bindingContext;
+const Page = require("tns-core-modules/ui/page").Page;
+const Label = require("tns-core-modules/ui/label").Label;
+const Button = require("tns-core-modules/ui/button").Button;
+const StackLayout = require("tns-core-modules/ui/layouts/stack-layout").StackLayout;
 
-    page.frame.navigate({
-        moduleName: "home/home-item-detail/home-item-detail-page",
-        context: tappedItem,
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 200,
-            curve: "ease"
-        }
+function onTap(args) {
+    let date = Date.now();
+    console.dir(date);
+
+    http.request({
+        url: "http://35.231.209.3/",
+        method: "GET"
+    }).then((res) => {
+        console.log(res['content']);
+        resp = res['content'];
+    }, (e) => {
+        console.log(e);
     });
+
+    
+    const view = args.object;
+    view.bindingContext = { hello: resp };
+    const context = null;
+    const closeCallback = null;
+    const fullscreen = true;
+    view.showModal("browse/browse-page", view.navigationContext, closeCallback, fullscreen);
 }
 
-exports.onItemTap = onItemTap;
+function createPage() {
+    // Creating of the page content
+    const stack = new StackLayout();
+    const label = new Label();
+    const button = new Button();
+
+    label.text = `You are number ${getCurrentCount()} in line!`;
+    button.text = "Check out!";
+    button.on(Button.tapEvent, (args) => onNavigatingTo(args), this);
+    stack.addChild(label);
+    stack.addChild(button);
+
+    const page = new Page();
+    // A page can have only one single root view set via the content property (in this case a StackLayout)
+    page.content = stack;
+
+    return page;
+}
+
+function getCurrentCount() {
+    return resp;
+}
+
+
+exports.onTap = onTap;
+exports.getCurrentCount = getCurrentCount;
+//exports.createPage = createPage;
 exports.onNavigatingTo = onNavigatingTo;
